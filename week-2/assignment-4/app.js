8573eb7b7c9d269e25e282aa8dcb7ba75fd350e3
Laura Wu <laura.jh.wu@gmail.com>
2021-03-04 21:49:07 +0800

    week-2 assign4: click to show and close menu

--- week-2/assignment-4/app.js
+++ week-2/assignment-4/app.js
@@ -7,3 +7,21 @@ const intro = document.querySelector('.intro');
 intro.addEventListener('click', () => {
     document.querySelector('.intro h2').innerText = "Have a Good Time!";
 })
+
+/* ==========================
+Request 2: click to show and close menu
+========================== */
+const header = document.querySelector('header');
+const mainNav = document.querySelector('.main-nav');
+
+header.addEventListener('click', event => {
+    // clicking on the hamburger shows and closes the navigation menu
+    if (event.target.matches('[id=hamburger]')) {
+        mainNav.classList.toggle('show-menu');
+    }
+    // clicking on the exit button in the navigation menu closes the menu
+    if (event.target.matches('.exit-btn')) {
+        mainNav.classList.remove('show-menu');
+    }
+})
+
