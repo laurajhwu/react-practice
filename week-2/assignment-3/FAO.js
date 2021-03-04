c500d626607ac9d749f0a1bfc0391e9e86acdba5
Laura Wu <laura.jh.wu@gmail.com>
2021-03-04 20:06:16 +0800

    week-2 assign3: initial commit

---
+++ week-2/assignment-3/FAO.js
@@ -0,0 +1,29 @@
+// A function to calculate the average price of all products
+function avg(data) {
+    // get the sum of the all product prices
+    let sum = 0;
+    data.products.map(item => sum += item.price);
+    // the average price is the sum of all prices divided by the amount of products
+    const average = sum / data.size;
+    //display and return average price
+    console.log(average);
+    return average;
+}
+
+avg({
+    size: 3,
+    products: [
+        {
+            name: "Product 1",
+            price: 100
+        },
+        {
+            name: "Product 2",
+            price: 700
+        },
+        {
+            name: "Product 3",
+            price: 250
+        }
+    ]
+}); // show the average price of all products
